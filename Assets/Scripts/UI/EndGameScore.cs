bc7007209def86f845b54dbc5bfbc43a1cb4d105
AntoniGoodday <32736437+AntoniGoodday@users.noreply.github.com>
2020-03-09 01:51:56 +0000

    Hopefully fixed I swear to god

--- Assets/Scripts/UI/EndGameScore.cs
+++ Assets/Scripts/UI/EndGameScore.cs
@@ -107,7 +107,7 @@ public class EndGameScore : MonoBehaviour
                     thePlayerData[usedPodiums].gameObject.SetActive(true);
                     thePlayerData[usedPodiums].GetComponent<PodiumController>().SetTotal(total);
                     usedPodiums++;
-                    SoundManager.Instance.SetBGM("Win");
+                    //SoundManager.Instance.SetBGM("Win");
                 }
             }
             //Disables all score texts
