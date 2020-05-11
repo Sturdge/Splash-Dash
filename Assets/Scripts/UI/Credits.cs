a95f72700e4c5a71aeb7ce52ba6dcaabd1f5d819
AlexWatsonn <55905700+AlexWatsonn@users.noreply.github.com>
2020-05-11 12:16:42 +0100

    Credits Scene and options menu
    Credits scene has been added and a button to go to it, is on the main menu
    Options menu has been implemented for all playable scenes
    
    Still a few tweaking to be done but mostly works as expected

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        if (Input.GetButtonDown("BackButton"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
--- Assets/Scripts/UI/MainMenu/NewMainMenu.cs
+++ Assets/Scripts/UI/MainMenu/NewMainMenu.cs
@@ -9,6 +9,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class NewMainMenu : MonoBehaviour
 {
@@ -19,6 +20,8 @@ public class NewMainMenu : MonoBehaviour
     private Transform confirmationMsg;
     [SerializeField]
     private Transform quitPanel;
+    [SerializeField]
+    private GameObject creditsBtn;
     [Header("Logic")]
     [SerializeField]
     private int selectId;
@@ -81,6 +84,7 @@ public class NewMainMenu : MonoBehaviour
         isTransition = false;
         confirmationMsg.gameObject.SetActive(false);
         quitPanel.gameObject.SetActive(false);
+        creditsBtn.gameObject.SetActive(true);
         canPressBtn = true;
         //Closed door 115.504 open door 24.249
         selectId = 0;
@@ -147,6 +151,7 @@ public class NewMainMenu : MonoBehaviour
         isTransition = true;
         canPressBtn = false;
         bool arrived = false;
+        creditsBtn.gameObject.SetActive(false);
         while (!arrived)
         {
             if (usingLerp == true)
@@ -197,6 +202,7 @@ public class NewMainMenu : MonoBehaviour
     private IEnumerator CameraSide()
     {
         previewingMedals = true;
+        creditsBtn.gameObject.SetActive(false);
         isTransition = true;
         canPressBtn = false;
         bool arrived = false;
@@ -222,6 +228,7 @@ public class NewMainMenu : MonoBehaviour
     private IEnumerator CameraControls()
     {
         previewingControls = true;
+        creditsBtn.gameObject.SetActive(false);
         isTransition = true;
         canPressBtn = false;
         bool arrived = false;
@@ -489,5 +496,11 @@ public class NewMainMenu : MonoBehaviour
         //canPressBtn = true;
 
         yield return null;
+    }
+
+    public void CreditsScene()
+    {
+        SceneManager.LoadScene(11);
+
     }
 }
