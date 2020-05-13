3267b993235b15510e51c12e6c75e9889a35e495
AntoniGoodday <32736437+AntoniGoodday@users.noreply.github.com>
2020-05-13 21:42:29 +0100

    splat sounds on countdown

--- Assets/Scripts/GameManager/CountdownTimer.cs
+++ Assets/Scripts/GameManager/CountdownTimer.cs
@@ -26,12 +26,17 @@ public class CountdownTimer : MonoBehaviour
 
     SoundManager soundManager;
 
+    AudioSource audio;
+
+    bool doneOne, doneTwo, doneThree, doneFour, doneFive;
+
     private void Start()
     {
         isCounting = true;
         countDown = 0;
         countdownVisual.sprite = countDownImages[countDown];
         countdownVisual.gameObject.SetActive(false);
+        audio = GetComponent<AudioSource>();
     }
 
     //handles countdown timer by changing between 4 sprites
@@ -41,34 +46,49 @@ public class CountdownTimer : MonoBehaviour
         {
             countdownVisual.sprite = countDownImages[countDown];
             timer += Time.deltaTime;
-            if (timer >= 1.0f && timer <= 1.99f)
+            if (timer >= 1.0f && timer <= 1.99f && doneOne == false)
             {
                 countdownVisual.gameObject.SetActive(true);
                 countDown = 0;
-                
+                audio.pitch = Random.Range(0.8f, 1);
+                audio.Play();
+                doneOne = true;
 
             }
-            else if (timer >= 2.0f && timer <= 2.99f)
+            else if (timer >= 2.0f && timer <= 2.99f && doneTwo == false)
             {
                 countDown = 1;
+                audio.pitch = Random.Range(0.8f, 1);
+                audio.Play();
+                doneTwo = true;
             }
-            else if (timer >= 3.0f && timer <= 3.99f)
+            else if (timer >= 3.0f && timer <= 3.99f && doneThree == false)
             {
                 countDown = 2;
+                audio.pitch = Random.Range(0.8f, 1);
+                audio.Play();
+                doneThree = true;
             }
-            else if (timer >= 4.0f && timer <= 5.0f)
+            else if (timer >= 4.0f && timer <= 5.0f && doneFour == false)
             {
                 countDown = 3;
+                audio.pitch = Random.Range(0.8f, 1);
+                audio.Play();
+                doneFour = true;
             }
-            else if (timer >= 5.1f)
+            else if (timer >= 5.1f && doneFive == false)
             {
                 countdownVisual.gameObject.SetActive(false);
                 ManageGame.instance.StartTimer();
                 isCounting = false;
                 soundManager = SoundManager.Instance;
                 soundManager.StartSong();
+                //audio.Play();
+                doneFour = true;
                 //soundManager.AudioSource.Play();
             }          
         }
     }
+
+    
 }
